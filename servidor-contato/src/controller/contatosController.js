a0d83b8d7a1d92e3d81079a500312ce90e6ec1f5
regianemaioli <regianemaioli@hotmail.com>
2019-11-06 22:26:15 -0300

    criando consulta de duplicidade

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -6,13 +6,24 @@ const getAll = (request, response) => {
 };
 
 
-const add = (request, response, ) => {
-  model.agenda.contatos.push(request.body)
-  response.status(200).send()
-}
+const add = (request, response) => {
+  let contato = request.body
+
+  contato.id = Math.random().toString(36).substr(-8)
+    console.log(contato)
+  model.agenda.contatos.push(contato)
+  
+  model.agenda.contatos.forEach(lista => {
+    console.log(lista)
+  })
+
+    response.status(200).send()
+  };
+
 
 module.exports = {
   getAll,
-  add
+  add,
+  
 }
 
